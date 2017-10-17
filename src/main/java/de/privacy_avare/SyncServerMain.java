336c1bd9bfd65cdbe5b145db161e6ec876b4a534
Lukas Struppek <lukas.struppek@gmail.com>
2017-10-17 08:52:42 +0200

    Adding Apache 2.0 License

--- src/main/java/de/privacy_avare/SyncServerMain.java
+++ src/main/java/de/privacy_avare/SyncServerMain.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Lukas Struppek.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.privacy_avare;
 
 import org.springframework.boot.SpringApplication;
@@ -24,6 +40,11 @@ public class SyncServerMain {
 	 *            Zusätzliche Parameter, die beim Programmstart übergeben werden.
 	 */
 	public static void main(String[] args) {
+		System.out.println("*****************************************************");
+		System.out.println("\t Copyright 2017 Lukas Struppek");
+		System.out.println("\t This Software is releases under version 2.0 of the Apache License");
+		System.out.println("\t See LICENSE.txt for further information");
+		System.out.println("*****************************************************");
 		SpringApplication app = new SpringApplication(SyncServerMain.class);
 		app.run(args);
 	}
